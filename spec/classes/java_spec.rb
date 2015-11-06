7388cef9f6ec6fc82395d51cd39e77ed473364f8
Parker, Peter G <parkerpg@ornl.gov>
2015-11-06 13:40:52 -0500

    Nest tests in describe block.

--- spec/classes/java_spec.rb
+++ spec/classes/java_spec.rb
@@ -4,23 +4,26 @@ describe 'icat::java' do
   let :pre_condition do
     "@package { 'wget': ensure => installed } @file { '/tmp': ensure => 'directory' }"
   end
-  let :params do
-    {'tmp_dir' => '/tmp'}
-  end
 
-  it 'should compile' do
-    should compile.with_all_deps()
+  describe 'with reasonable tmp_dir param value' do
+    let :params do
+      {'tmp_dir' => '/tmp'}
+    end
 
-    should create_class('icat::java')
-  end
+    it 'should compile' do
+      should compile.with_all_deps()
 
-  it 'should require the tmp_dir to download java' do
-    should contain_exec('download_jdk').that_requires('File[/tmp]')
-  end
+      should create_class('icat::java')
+    end
 
-  it 'should download and install a jdk package' do
-    should contain_package('jdk.x86_64').with({
-      'ensure' => 'installed',
-    }).that_requires('Exec[download_jdk]')
+    it 'should require the tmp_dir to download java' do
+      should contain_exec('download_jdk').that_requires('File[/tmp]')
+    end
+
+    it 'should download and install a jdk package' do
+      should contain_package('jdk.x86_64').with({
+        'ensure' => 'installed',
+      }).that_requires('Exec[download_jdk]')
+    end
   end
 end
