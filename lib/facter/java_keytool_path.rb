190e79f1bcc8443952fa5542828a0cd96505608f
Parker, Peter G <parkerpg@ornl.gov>
2015-11-08 14:52:30 -0500

    Change path resolution to use Ruby's inbuilt Path.join() funciton.

--- lib/facter/java_keytool_path.rb
+++ lib/facter/java_keytool_path.rb
@@ -3,6 +3,6 @@
 # Purpose: get the path to the security directory
 Facter.add(:java_keytool_path) do
   setcode do
-    Facter.value(:java_jre_path) + '/bin/keytool'
+    File.join(Facter.value(:java_jre_path), 'bin', 'keytool')
   end
 end
--- lib/facter/java_security_path.rb
+++ lib/facter/java_security_path.rb
@@ -3,6 +3,6 @@
 # Purpose: get the path to the security directory
 Facter.add(:java_security_path) do
   setcode do
-    Facter.value(:java_jre_path) + '/lib/security'
+    File.join(Facter.value(:java_jre_path), 'lib', 'security')
   end
 end
