8d67877417e7de18c30cf5f8055d0dac1f465d4a
Parker, Peter G <parkerpg@ornl.gov>
2015-11-08 13:51:09 -0500

    Reshuffle for more idiomatic use of default parameter definitions in specs.

--- spec/classes/appserver_spec.rb
+++ spec/classes/appserver_spec.rb
@@ -6,16 +6,21 @@ describe 'icat::appserver' do
     { :osfamily => 'RedHat' }
   end
 
-  describe 'with reasonable param values' do
-    let :params do
-      {
-        'tmp_dir'               => '/tmp',
-        'user'                  => 'username',
-        'group'                 => 'groupname',
-        'admin_password'        => 'p4ssw0rd',
-        'admin_master_password' => 'master_p4ssw0rd',
-        'db_type'               => 'oracle',
-      }
+  let :default_params do
+    {
+      'tmp_dir'               => '/tmp',
+      'user'                  => 'username',
+      'group'                 => 'groupname',
+      'admin_password'        => 'p4ssw0rd',
+      'admin_master_password' => 'master_p4ssw0rd',
+    }
+  end
+
+  context 'with reasonable param values and db_type of oracle' do
+    let(:params) do
+      default_params.merge({
+        :db_type => 'oracle'
+      })
     end
 
     it 'should compile' do
@@ -33,30 +38,35 @@ describe 'icat::appserver' do
         .that_requires('Class[glassfish]')
     end
 
-    describe 'and with a db_type param of mysql' do
-      let :params do
-        { 'db_type' => 'mysql' }
-      end
-      it 'should install the MySQL connector jar' do
-        should contain_glassfish__install_jars('mysql-connector-java-5.1.36-bin.jar')
-          .that_requires('Class[glassfish]')
-      end
-    end
-
-    describe 'and with an unknown database type' do
-      let :params do
-        { 'db_type' => 'DBX' }
-      end
-
-      it 'should raise an error' do
-        should compile.and_raise_error(/Unknown database type of 'DBX'/)
-      end
-    end
-
     it 'should create the icat domain and service' do
       should contain_glassfish__create_domain('icat').with({
         'create_service' => 'true',
       }).that_requires('Class[glassfish]')
+    end
+  end
+
+  context 'with a db_type param of mysql' do
+    let(:params) do
+      default_params.merge({
+        :db_type => 'mysql'
+      })
+    end
+
+    it 'should install the MySQL connector jar' do
+      should contain_glassfish__install_jars('mysql-connector-java-5.1.36-bin.jar')
+        .that_requires('Class[glassfish]')
+    end
+  end
+
+  context 'with an unknown database type' do
+    let(:params) do
+      default_params.merge({
+        :db_type => 'DBX'
+      })
+    end
+
+    it 'should raise an error' do
+      should compile.and_raise_error(/Unknown database type of 'DBX'/)
     end
   end
 end
