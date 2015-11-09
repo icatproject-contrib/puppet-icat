a32b949114160aae458f74dd6f895be55847b69c
Parker, Peter G <parkerpg@ornl.gov>
2015-11-09 11:32:56 -0500

    Workaround the problem experienced by clean builds that do a fresh provision of the VM first.  Incremental builds were passing fine, but this was because the application server had aleady been installed by a previous run of the tests, and this meant the Puppet fact was returning successfully.  Starting to get the hang of Puppet, but it's still a painful process...

--- lib/facter/appserver_path.rb
+++ lib/facter/appserver_path.rb
@@ -3,6 +3,13 @@
 # Purpose: get the path to the installed application server
 Facter.add(:appserver_path) do
   setcode do
-    File.expand_path(File.join(Facter::Util::Resolution.which('asadmin'), '..', '..', '..'))
+    # I think a fact is the wrong mechanism for delivering this infomation dynamically
+    # (for example by executing 'which asadmin' on the command line) since facts are
+    # evaluated at manifest compile time -- before the app server has been installed.
+    # I guess there's no reason why we can't just pass an 'install_dir' param to the
+    # glassfish module (or wildfly if/when we migrate) and then use the same string
+    # for whatever needs this fact.
+    # In any event, this is a quick and brittle workaround which will do for now.
+    '/usr/local/glassfish-4.0'
   end
 end
--- spec/unit/facter/appserver_path_spec.rb
+++ spec/unit/facter/appserver_path_spec.rb
@@ -8,8 +8,6 @@ describe Facter::Util::Fact do
   describe "appserver_path" do
     context 'returns the path of the application server' do
       it do
-        Facter::Util::Resolution.expects(:which).with("asadmin").returns('/usr/local/glassfish-4.0/glassfish/bin/asadmin')
-
         expect(Facter.value(:appserver_path)).to eq('/usr/local/glassfish-4.0')
       end
     end
