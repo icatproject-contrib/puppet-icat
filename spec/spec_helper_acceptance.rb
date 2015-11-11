e8f5e65d85ed745965252ce0817cff4ca459b6e2
Parker, Peter G <parkerpg@ornl.gov>
2015-11-11 16:04:45 -0500

    Workaround for weird symlinking problem.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,6 +2,8 @@ require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 require 'beaker/librarian'
 
+require 'fileutils'
+
 RSpec.configure do |c|
   module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
@@ -28,6 +30,14 @@ RSpec.configure do |c|
     #   # ... Always (re-)install modules however, since they may have changed.
     #   librarian_install_modules(module_root, 'icat')
     # end
+
+    # And the plot thickens.  For some reason the symlinking functionality of librarian that
+    # symlinks [MODULE_DIR] to [MODULE_DIR]/spec/fixtures/modules/icat/ so that the module
+    # is available to the acceptance tests seems to link round and round recursively, once
+    # for each run of the tests.  It's not clear what's causing this.  Let's just delete it
+    # here to make sure we're starting afresh each time.
+    icat_symlink = File.join(module_root, 'spec', 'fixtures', 'modules', 'icat')
+    FileUtils.rm icat_symlink, :force => true
 
     install_puppet
     install_librarian
