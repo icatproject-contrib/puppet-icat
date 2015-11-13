1015e715adf0a7eaf49ccf91792ebec2bcf8c146
Parker, Peter G <parkerpg@ornl.gov>
2015-11-13 17:25:46 -0500

    Remove TODO.
    I think this was not working since librarian would only be installed if Puppet wasn't installed ... which it of course was since we are using a CentOS7 box from Puppetlabs.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -12,25 +12,6 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
 
-    # TODO: Consider the following nice-to-have, which for some reason breaks
-    #       breaks the modulepath workaround in the acceptance test file.
-    #
-    # hosts.each do |host|
-    #   # Don't bother re-installing Puppet and librarian if they are already installed,
-    #   # which could happen given the BEAKER_provision env var (see /README.md).
-    #   # Idea taken from https://github.com/maestrodev/puppet-maven/blob/master/spec/spec_helper_acceptance.rb
-    #   unless (ENV['BEAKER_provision'] == 'no')
-    #     begin
-    #       on host, 'puppet --version'
-    #     rescue
-    #       install_puppet
-    #       install_librarian
-    #     end
-    #   end
-    #   # ... Always (re-)install modules however, since they may have changed.
-    #   librarian_install_modules(module_root, 'icat')
-    # end
-
     # And the plot thickens.  For some reason the symlinking functionality of librarian that
     # symlinks [MODULE_DIR] to [MODULE_DIR]/spec/fixtures/modules/icat/ so that the module
     # is available to the acceptance tests seems to link round and round recursively, once
