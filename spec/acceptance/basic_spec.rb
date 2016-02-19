cea2653e710c5d94cd5b61220e072b0e8d6cd404
Parker, Peter G <parkerpg@ornl.gov>
2016-02-19 15:08:34 -0500

    Make sure any issues that affect basic CRUD functionality of ICAT cause the acceptance tests to fail.

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -133,5 +133,9 @@ describe 'the icat class' do
     it 'should have created a security cert against the appropriate hostname' do
       shell('grep "icat-puppet-test" /usr/java/jdk1.7.0_79/jre/lib/security/jssecacerts', :acceptable_exit_codes => 0)
     end
+
+    it 'should enable testicat to pass without problems' do
+      shell('/home/vagrant/icat/bin/testicat https://localhost:4881 simple username user_a password password_a', :acceptable_exit_codes => 0)
+    end
   end
 end
